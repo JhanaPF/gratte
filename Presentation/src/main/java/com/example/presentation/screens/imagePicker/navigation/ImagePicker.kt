45425bf5dfaf17b6e4eba0988c34389c56e56522
Cyril <cyril.puccio@epitech.eu>
2025-03-15 17:50:58 +0100

    Fix offuscation

--- Presentation/src/main/java/com/example/presentation/screens/imagePicker/navigation/ImagePicker.kt
+++ Presentation/src/main/java/com/example/presentation/screens/imagePicker/navigation/ImagePicker.kt
@@ -1,12 +1,15 @@
 package com.example.presentation.screens.imagePicker.navigation
 
+import androidx.annotation.Keep
 import kotlinx.serialization.Serializable
 
 // I Had to put it there to be able to use the savedStateHandle.toRoute because if it stays in AppRoute
 // i would create a circle dependency between Navigation and Presentation
+@Keep
 @Serializable
 data class ImagePicker(val origin: Origin = Origin.NAVBAR) {
     companion object {
+        @Keep
         enum class Origin {
             GALLERY,
             NAVBAR,
--- Presentation/src/main/java/com/example/presentation/screens/imageView/navigation/ImageViewNavigation.kt
+++ Presentation/src/main/java/com/example/presentation/screens/imageView/navigation/ImageViewNavigation.kt
@@ -1,8 +1,10 @@
 package com.example.presentation.screens.imageView.navigation
 
+import androidx.annotation.Keep
 import kotlinx.serialization.Serializable
 
 // I Had to put it there to be able to use the savedStateHandle.toRoute because if it stays in AppRoute
 // i would create a circle dependency between Navigation and Presentation
+@Keep
 @Serializable
 data class ImageView(val imageId: Int)
